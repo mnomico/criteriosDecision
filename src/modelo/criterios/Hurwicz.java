81d6601f7a081105d0af174900b40aad62a48faa
mnomico <nomicomateou@gmail.com>
2025-04-12 16:17:23 -0300

    add Hurwicz criterion implementation

--- src/modelo/criterios/Hurwicz.java
+++ src/modelo/criterios/Hurwicz.java
@@ -1,9 +1,11 @@
 package modelo.criterios;
 
-import modelo.TablaDatos;
+import modelo.MatrizBeneficios;
 
 public class Hurwicz implements ICriterio {
     private final double alfa;
+    private double[] indicadores;
+    private int mejorAlternativa;
 
     public Hurwicz(double alfa) {
         this.alfa = alfa;
@@ -13,8 +15,38 @@ public class Hurwicz implements ICriterio {
         this.alfa = 0.5;
     }
 
-    public void calcularDecision(TablaDatos tablaDatos) {
-        // TODO implementación
+    public int calcularAlternativa(MatrizBeneficios matrizBeneficios) {
+        int alternativas = matrizBeneficios.getCantidadAlternativas();
+        int estados = matrizBeneficios.getCantidadEstados();
+        indicadores = new double[alternativas];
+        for (int i = 0; i < alternativas; i++) {
+            double maximo = matrizBeneficios.getValor(i, 0);
+            double minimo = maximo;
+            for (int j = 1; j < estados; j++) {
+                double valorActual = matrizBeneficios.getValor(i, j);
+                if (valorActual < minimo) {
+                    minimo = valorActual;
+                } else if (valorActual > maximo) {
+                    maximo = valorActual;
+                }
+            }
+            indicadores[i] = (alfa * maximo) + (1 - alfa) * minimo;
+        }
+
+        double maximo = indicadores[0];
+        mejorAlternativa = 0;
+        for (int i = 1; i < indicadores.length; i++) {
+            if (indicadores[i] > maximo) {
+                maximo = indicadores[i];
+                mejorAlternativa = i;
+            }
+        }
+
+        return mejorAlternativa;
+    }
+
+    public Object getIndicadores() {
+        return indicadores;
     }
 
     public String toString() {
