9f3e594ca19ee10d5c2efbbdc48ba192ed0d6b16
mnomico <nomicomateou@gmail.com>
2025-04-12 15:56:48 -0300

    add Maximax criterion implementation

--- src/modelo/criterios/Maximax.java
+++ src/modelo/criterios/Maximax.java
@@ -1,11 +1,39 @@
 package modelo.criterios;
 
-import modelo.TablaDatos;
+import modelo.MatrizBeneficios;
 
 public class Maximax implements ICriterio {
+    private double indicadores[];
+    private int alternativa;
 
-    public void calcularDecision(TablaDatos tablaDatos) {
-        // TODO implementación
+    public int calcularDecision(MatrizBeneficios matrizBeneficios) {
+        int alternativas = matrizBeneficios.getCantidadAlternativas();
+        int estados = matrizBeneficios.getCantidadEstados();
+        indicadores = new double[alternativas];
+        for (int i = 0; i < alternativas; i++) {
+            double maximo = matrizBeneficios.getValor(i, 0);
+            for (int j = 1; j < estados; j++) {
+                double valorActual = matrizBeneficios.getValor(i, j);
+                if (valorActual > maximo) {
+                    maximo = valorActual;
+                }
+            }
+            indicadores[i] = maximo;
+        }
+
+        double maximo = indicadores[0];
+        for (int i = 1; i < indicadores.length; i++) {
+            if (indicadores[i] > maximo) {
+                maximo = indicadores[i];
+                alternativa = i;
+            }
+        }
+
+        return alternativa;
+    }
+
+    public Object getIndicadores() {
+        return indicadores;
     }
 
     public String toString() {
