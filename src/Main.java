c2e113a9150521be92c741cbdb53634ec73afd70
mnomico <nomicomateou@gmail.com>
2025-04-17 20:10:54 -0300

    add controller

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
-public class Main {
+import controlador.Controlador;
 
+public class Main {
+    public static void main(String[] args) {
+        Controlador controlador = new Controlador();
+    }
 }
