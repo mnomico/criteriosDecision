fc81127a2c3a7bb694ddca251fb61046b71acd59
mnomico <nomicomateou@gmail.com>
2025-04-17 20:11:56 -0300

    add model methods

--- src/modelo/IModeloDecision.java
+++ src/modelo/IModeloDecision.java
@@ -1,4 +1,19 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public interface IModeloDecision {
+    void definirTamanioMatriz(int decisiones, int estados);
+
+    void cargarMatriz(double[][] datos) throws Exception;
+
+    void actualizarDato(int decision, int estado, double valor);
+
+    void actualizarDecision(int decision, String nombreDecision);
+
+    void actualizarEstado(int estado, String nombreEstado);
+
+    ArrayList<String> getCriterios();
+
+    void calcularCriterios(ArrayList<String> criteriosSeleccionados) throws Exception;
 }
