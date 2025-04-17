1b3a9c8b1865fc02bebcbe938199c359a3060f24
mnomico <nomicomateou@gmail.com>
2025-04-17 20:11:12 -0300

    add buscarCriterio method

--- src/modelo/criterios/ListaCriterios.java
+++ src/modelo/criterios/ListaCriterios.java
@@ -10,4 +10,13 @@ public class ListaCriterios {
             new Savage()
         );
     }
+
+    public static ICriterio buscarCriterio(String nombre) {
+        for (ICriterio criterio : getCriterios()) {
+            if (criterio.toString().equalsIgnoreCase(nombre)) {
+                return criterio;
+            }
+        }
+        return null;
+    }
 }
