94ac1c7a9de4d96ba8e3921577a0aa6a3c24ea16
mnomico <nomicomateou@gmail.com>
2025-04-17 20:11:39 -0300

    add view methods up to VentanaCriterios

--- src/vista/IVista.java
+++ src/vista/IVista.java
@@ -1,4 +1,35 @@
 package vista;
 
+import java.util.ArrayList;
+
 public interface IVista {
+
+    public void mostrarMensaje(String mensaje);
+
+    public void mostrarAnterior();
+
+    public void mostrarSiguiente();
+
+    public String getVentanaActual();
+
+    /// Métodos de Ventanas
+
+    // Métodos de VentanaInicio
+
+    public String getAlternativasVentanaInicio();
+
+    public String getEstadosVentanaInicio();
+
+    // Métodos de VentanaMatriz
+
+    public void armarMatrizVentanaMatriz(int alternativas, int estados);
+
+    public double[][] retornarValoresMatrizVentanaMatriz() throws NumberFormatException;
+
+    // Métodos de VentanaCriterios
+
+    public void mostrarCriterios(ArrayList<String> criterios);
+
+    public ArrayList<String> retornarCriteriosSeleccionados();
+
 }
